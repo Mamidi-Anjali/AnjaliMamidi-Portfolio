44ae6a8decc12ff89ab2a071cebb0e80d9be769d
Anjali Mamidi <mamidianjali99@gmail.com>
2024-06-13 08:46:27 -0400

    added a new project details - prospective student feedback app

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import TicTacToe from "../../Assets/Projects/TictactoePicture.png"
-import Portfolio from "../../Assets/Projects/Portfolio.png"
+import TicTacToe from "../../Assets/Projects/TictactoePicture.png";
+import Portfolio from "../../Assets/Projects/Portfolio.png";
+import StudentSurvey from "../../Assets/Projects/StudentSurvey.png";
 
 function Projects() {
   return (
@@ -24,7 +25,6 @@ function Projects() {
               title="TIC-TAC-TOE"
               description="This project is a web-based implementation of the classic Tic-Tac-Toe game using React js. The game allows two players to take turns placing their respective markers (crosses and circles) on a 3x3 grid. The first player to align three of their markers horizontally, vertically, or diagonally wins the game. The game includes a simple user interface with interactive elements and provides feedback for game actions."
               ghLink="https://github.com/Mamidi-Anjali/TIC-TAC-TOE"
-              // demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
@@ -34,8 +34,19 @@ function Projects() {
               isBlog={false}
               title="Anjali Mamidi - Portfolio"
               description="This personal portfolio website is a showcase of my skills, projects, and experiences as a full-stack web developer. Developed using React.js, the website features a modern, responsive design and provides detailed information about my background, technical expertise, and accomplishments."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/Mamidi-Anjali/AnjaliMamidi-Portfolio"
+            />
+          </Col>
+
+          <Col xs={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={StudentSurvey}
+              isBlog={false}
+              title="Prospective Student Feedback Application"
+              description=" Developed a full-stack web application using Angular and Spring Boot with JPA, providing a seamless survey experience 
+for prospective students. Integrated RESTful APIs for efficient form submissions and data retrieval. Employed JPA for 
+robust and scalable database interactions. "
+              ghLink="https://github.com/Mamidi-Anjali/ProspectiveStudentFeedbackApplication"
             />
           </Col>
 {/* 
@@ -45,8 +56,7 @@ function Projects() {
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/Mamidi-Anjali/AnjaliMamidi-Portfolio"
             />
           </Col>
 
@@ -56,8 +66,7 @@ function Projects() {
               isBlog={false}
               title="Plant AI"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/Mamidi-Anjali/AnjaliMamidi-Portfolio"
             />
           </Col>
 
@@ -67,8 +76,7 @@ function Projects() {
               isBlog={false}
               title="Ai For Social Good"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              ghLink="https://github.com/Mamidi-Anjali/AnjaliMamidi-Portfolio"
             />
           </Col>
 
@@ -79,8 +87,7 @@ function Projects() {
               title="Face Recognition and Emotion Detection"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink="https://github.com/Mamidi-Anjali/AnjaliMamidi-Portfolio"
             />
           </Col>*/}
         </Row> 
